af25bbbbf3cb2f1daf2fe098b4431ef591dc373f
Yauhenii <yauhenmardan@gmail.com>
2021-09-22 13:18:47 +0300

    add opt contracts, price data scrapper

--- app/factories/yahooscrapper_factory.py
+++ app/factories/yahooscrapper_factory.py
@@ -1,11 +1,6 @@
 from app import YahooScrapper
-from app import settings
 
 
 def get_yahooscrapper() -> YahooScrapper:
-    host = settings.yahooscrapper.HOST
-    path_template = settings.yahooscrapper.PATH_TEMPLATE
-    user_agent = settings.yahooscrapper.USER_AGENT
-
-    scrapper = YahooScrapper(host, path_template, user_agent)
+    scrapper = YahooScrapper()
     return scrapper
